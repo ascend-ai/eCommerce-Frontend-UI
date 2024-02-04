81071a8c2eb3e70b1c5805f6b42b1591d632e5b1
Yash Desai <yashdesoi@gmail.com>
2024-02-04 12:48:37 +0530

    Primary colour of theme is changed to Pink.

--- src/core/interfaces/pagination.interface.ts
+++ src/core/interfaces/pagination.interface.ts
@@ -1,7 +1,22 @@
+import {
+  BASE_SORTABLE_COLUMN,
+  ORDER_SORTABLE_COLUMN,
+  PRODUCT_SORTABLE_COLUMN,
+  USER_SORTABLE_COLUMN
+} from '../constants';
+import {
+  SortDirection
+} from '../enums';
+
 export interface PaginationInterface<T> {
   content: Array<T>;
   totalElements: number;
   totalPages: number;
   page: number;
   size: number;
+  sortColumn: (keyof typeof BASE_SORTABLE_COLUMN) |
+              (keyof typeof PRODUCT_SORTABLE_COLUMN) |
+              (keyof typeof ORDER_SORTABLE_COLUMN) |
+              (keyof typeof USER_SORTABLE_COLUMN)
+  sortDirection: SortDirection;
 };
