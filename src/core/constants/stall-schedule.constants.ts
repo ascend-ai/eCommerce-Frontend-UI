728e5ad2bca69d4b9919775b71c39dc44e886e89
Yash Desai <yashdesoi@gmail.com>
2024-04-22 16:41:55 +0530

    Added constants for Product and Stall schedule validation.

--- src/core/constants/stall-schedule.constants.ts
+++ src/core/constants/stall-schedule.constants.ts
@@ -2,6 +2,10 @@ import {
   BASE_SORTABLE_COLUMN
 } from './';
 
+export const STALL_SCHEDULE_VENUE_LENGTH_RANGE = {
+  MIN: 5,
+} as const;
+
 export const STALL_SCHEDULE_SORTABLE_COLUMN = {
   ...BASE_SORTABLE_COLUMN,
   date: 'date',
