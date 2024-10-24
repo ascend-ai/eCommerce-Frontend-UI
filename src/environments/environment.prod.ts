dab640048396cf4cdad3ccca4423f7c3a6c5b268
Yash Desai <yashdesoi@gmail.com>
2024-10-24 12:53:12 +0530

    Added razorpay's live configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl: 'https://ecommerce-backend-api-ss4r.onrender.com',
   apiUrl: 'https://ecommerce-backend-api-ss4r.onrender.com/api/v1',
-  razorpayId: 'rzp_test_UIH6naCSSAkS18',
+  razorpayId: 'rzp_live_K7fXHB8SJcLEWB',
 };
